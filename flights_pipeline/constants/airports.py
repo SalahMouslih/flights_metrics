48ea94284efbdb824792527ece59f2c5fe1410aa
salahddin <salah-eddine.elmouslih@ensae.fr>
2025-06-09 22:23:24 +0100

    feat: add constants module for fixed values

---
+++ flights_pipeline/constants/airports.py
@@ -0,0 +1,6 @@
+ILE_DE_FRANCE_AIRPORTS = [
+    {"iata": "CDG", "lat": 49.0097, "lon": 2.5479},
+    {"iata": "ORY", "lat": 48.7262, "lon": 2.3652},
+    {"iata": "BVA", "lat": 49.4544, "lon": 2.1128},
+    {"iata": "LBG", "lat": 48.9694, "lon": 2.4414},
+]
